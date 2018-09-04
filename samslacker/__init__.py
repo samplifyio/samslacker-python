51abe8ab748fdfa9895a047bea82883c042f95a4
Charlie Mak <charlie@samplify.io>
2018-09-04 21:28:15 +0100

    version 0.0.6.3 - properly set region name

--- samslacker/__init__.py
+++ samslacker/__init__.py
@@ -6,6 +6,7 @@ from samslacker.resources import Events
 
 api_base = 'https://samplify.io/slacker/api'
 sns_arn = 'arn:aws:sns:us-east-1:319868825377:sam-slacker-event-created'
+aws_region = 'us-east-1'
 api_version = 'v1'
 token = None
 project = None
@@ -23,7 +24,7 @@ def event(_name, *args, **kwargs):
     if project is not None:
         data['project_id'] = project
 
-    sns_resource = boto3.resource('sns')
+    sns_resource = boto3.resource('sns', region_name=aws_region)
     return sns_resource.Topic(sns_arn).publish(
         Message=json.dumps(data),
     )
